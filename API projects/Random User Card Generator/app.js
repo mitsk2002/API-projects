76a33b467217e0e406d53e3417bc0347e253e33b
mitsk2002 <31905698+mitsk2002@users.noreply.github.com>
2025-08-14 17:20:42 -0500

    Update app.js
    Used the stable randomuser.me API

--- API projects/Random User Card Generator/app.js
+++ API projects/Random User Card Generator/app.js
@@ -1,22 +1,31 @@
-let details = document.querySelector(".details");
-let imgContainer = document.querySelector(".img-container");
-let getUserBtn = document.getElementById("get-user-btn");
-let url = "https://random-data-api.com/api/v2/users?response_type=json";
+const details = document.querySelector(".details");
+const imgContainer = document.querySelector(".img-container");
+const getUserBtn = document.getElementById("get-user-btn");
+const url = "https://randomuser.me/api/";
 
-let getUser = () => {
-    fetch(url)
-        .then((resp) => resp.json())
-        .then((data) => {
-            imgContainer.innerHTML = `<img src=${data.avatar}>`;
-            details.innerHTML = `
-            <h2>${data.first_name} ${data.last_name}</h2>
-            <h3>${data.employment.title}</h3>
-            <h4><i class="fa-solid fa-location-dot"></i>${data.address.city}</h4>`;
-        
-            let randomColor = "#" + ((Math.random() * 0xffffff) << 0).toString(16).padStart(6, "0");
-            document.documentElement.style.setProperty("--theme-color", randomColor);
-       
-        });
+const getUser = () => {
+  fetch(url)
+    .then((resp) => resp.json())
+    .then((data) => {
+      const user = data.results[0];
+
+      // Set image and user details
+      imgContainer.innerHTML = `<img src="${user.picture.large}" alt="User Avatar">`;
+      details.innerHTML = `
+        <h2>${user.name.first} ${user.name.last}</h2>
+        <h3>${user.login.username}</h3>
+        <h4><i class="fa-solid fa-location-dot"></i> ${user.location.city}, ${user.location.country}</h4>
+      `;
+
+      // Set a random theme color
+      const randomColor = "#" + ((Math.random() * 0xffffff) << 0).toString(16).padStart(6, "0");
+      document.documentElement.style.setProperty("--theme-color", randomColor);
+    })
+    .catch((err) => console.error("Fetch error:", err));
 };
+
+// Load a user when page loads
 window.addEventListener("load", getUser);
+
+// Load a new user when button is clicked
 getUserBtn.addEventListener("click", getUser);
